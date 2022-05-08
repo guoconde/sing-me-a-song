e6b9f693078bf2f77e01e38d45fc2b6beab0873e
guoconde <guoconde@gmail.com>
2022-05-08 18:14:32 -0300

    Feat: downvote unit test

--- back-end/tests/units/service.test.ts
+++ back-end/tests/units/service.test.ts
@@ -1,7 +1,8 @@
 import { jest } from '@jest/globals';
 import { recommendationRepository } from '../../src/repositories/recommendationRepository.js';
 import { recommendationService } from '../../src/services/recommendationsService.js';
-import { notFoundError } from '../../src/utils/errorUtils';
+import { notFoundError } from '../../src/utils/errorUtils.js';
+import servicesFactory from '../factories/servicesFactory.js';
 
 describe('Recommendations service', () => {
   beforeEach(() => {
@@ -24,5 +25,29 @@ describe('Recommendations service', () => {
     expect(async () => {
       await recommendationService.upvote(1);
     }).rejects.toEqual(notFoundError());
+  });
+
+  it('Should return notFoundError when there arent id recommendations on downvote', () => {
+    jest.spyOn(recommendationRepository, 'find').mockResolvedValue(null);
+
+    expect(async () => {
+      await recommendationService.downvote(1);
+    }).rejects.toEqual(notFoundError());
+  });
+
+  console.log('123');
+  it('Should remove recommendation on score is lower then -5 on downvote', async () => {
+    const body = servicesFactory()[2];
+
+    jest.spyOn(recommendationRepository, 'find').mockResolvedValue(body);
+    jest.spyOn(recommendationRepository, 'updateScore').mockResolvedValue(body);
+
+    const remove = jest.spyOn(recommendationRepository, 'remove').mockResolvedValue(null);
+
+    await recommendationService.downvote(body.id);
+
+    expect(remove).toHaveBeenCalledTimes(1);
+    expect(recommendationRepository.updateScore).toBeCalledWith(body.id, 'decrement');
+    expect(recommendationRepository.remove).toBeCalledWith(body.id);
   });
 });
