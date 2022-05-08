225033e201404b446c00389e276841c75904e75a
guoconde <guoconde@gmail.com>
2022-05-08 15:59:07 -0300

    Feat: Inserting Get/recommendations and random  ROUTE

--- back-end/tests/index.test.ts
+++ back-end/tests/index.test.ts
@@ -1,12 +1,16 @@
 import supertest from 'supertest';
+import { prisma } from '../src/database.js';
 import app from '../src/app.js';
 import { CreateRecommendationData } from '../src/services/recommendationsService.js';
 import { prismaDisconnect, truncateTable } from './utils/functionsUtils.js';
 
-describe('POST /recommendations', () => {
+function onStart() {
   beforeEach(truncateTable);
-
   afterAll(prismaDisconnect);
+}
+
+describe('POST /recommendations', () => {
+  onStart();
 
   it('Should return 201 on a valid body', async () => {
     const body: CreateRecommendationData = {
@@ -17,6 +21,18 @@ describe('POST /recommendations', () => {
     const response = await supertest(app).post('/recommendations').send(body);
 
     expect(response.status).toEqual(201);
+  });
+
+  it('Should return 409 on conflit valid body', async () => {
+    const body: CreateRecommendationData = {
+      name: '10 Coisas IMPRESSIONANTES Que Encontrei na Internet!!',
+      youtubeLink: 'https://www.youtube.com/watch?v=m26jErLd5ds',
+    };
+
+    await supertest(app).post('/recommendations').send(body);
+    const response = await supertest(app).post('/recommendations').send(body);
+
+    expect(response.status).toEqual(409);
   });
 
   it('Should return 422 when unnamed body', async () => {
@@ -43,5 +59,43 @@ describe('POST /recommendations', () => {
     const response = await supertest(app).post('/recommendations');
 
     expect(response.status).toEqual(422);
+  });
+});
+
+describe('GET /recommendations', () => {
+  onStart();
+
+  it('Should return existent arry on get is done and status 200', async () => {
+    const body: CreateRecommendationData = {
+      name: '10 Coisas IMPRESSIONANTES Que Encontrei na Internet!!',
+      youtubeLink: 'https://www.youtube.com/watch?v=m26jErLd5ds',
+    };
+
+    await prisma.recommendation.create({ data: { ...body } });
+
+    const response = await supertest(app).get('/recommendations');
+
+    expect(response.status).toEqual(200);
+    expect(response.body.length).toBeGreaterThan(0);
+    expect(response.body.length).not.toBeNull();
+  });
+});
+
+describe('GET /recommendations/random', () => {
+  onStart();
+
+  it('Should return existent object on get a random recommendation', async () => {
+    const body: CreateRecommendationData = {
+      name: '10 Coisas IMPRESSIONANTES Que Encontrei na Internet!!',
+      youtubeLink: 'https://www.youtube.com/watch?v=m26jErLd5ds',
+    };
+
+    const create = await prisma.recommendation.create({ data: { ...body } });
+
+    console.log(create);
+
+    const response = await supertest(app).get('/recommendations/random');
+
+    expect(response.body).toEqual(create);
   });
 });
