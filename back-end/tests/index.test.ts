fdde6e86f3421cf33fc79c48a0a8da0a1ebac40f
guoconde <guoconde@gmail.com>
2022-05-08 16:51:42 -0300

    Fix: refacturing using bodyFactory

--- back-end/tests/index.test.ts
+++ back-end/tests/index.test.ts
@@ -3,6 +3,7 @@ import { prisma } from '../src/database.js';
 import app from '../src/app.js';
 import { CreateRecommendationData } from '../src/services/recommendationsService.js';
 import { prismaDisconnect, truncateTable } from './utils/functionsUtils.js';
+import bodyFactory from './factories/bodyFactory.js';
 
 function onStart() {
   beforeEach(truncateTable);
@@ -13,10 +14,7 @@ describe('POST /recommendations', () => {
   onStart();
 
   it('Should return 201 on a valid body', async () => {
-    const body: CreateRecommendationData = {
-      name: '10 Coisas IMPRESSIONANTES Que Encontrei na Internet!!',
-      youtubeLink: 'https://www.youtube.com/watch?v=m26jErLd5ds',
-    };
+    const body: CreateRecommendationData = bodyFactory()[0];
 
     const response = await supertest(app).post('/recommendations').send(body);
 
@@ -24,10 +22,7 @@ describe('POST /recommendations', () => {
   });
 
   it('Should return 409 on conflit valid body', async () => {
-    const body: CreateRecommendationData = {
-      name: '10 Coisas IMPRESSIONANTES Que Encontrei na Internet!!',
-      youtubeLink: 'https://www.youtube.com/watch?v=m26jErLd5ds',
-    };
+    const body: CreateRecommendationData = bodyFactory()[0];
 
     await supertest(app).post('/recommendations').send(body);
     const response = await supertest(app).post('/recommendations').send(body);
@@ -66,10 +61,7 @@ describe('GET /recommendations', () => {
   onStart();
 
   it('Should return existent arry on get is done and status 200', async () => {
-    const body: CreateRecommendationData = {
-      name: '10 Coisas IMPRESSIONANTES Que Encontrei na Internet!!',
-      youtubeLink: 'https://www.youtube.com/watch?v=m26jErLd5ds',
-    };
+    const body: CreateRecommendationData = bodyFactory()[0];
 
     await prisma.recommendation.create({ data: { ...body } });
 
@@ -85,10 +77,7 @@ describe('GET /recommendations/random', () => {
   onStart();
 
   it('Should return existent object on get a random recommendation', async () => {
-    const body: CreateRecommendationData = {
-      name: '10 Coisas IMPRESSIONANTES Que Encontrei na Internet!!',
-      youtubeLink: 'https://www.youtube.com/watch?v=m26jErLd5ds',
-    };
+    const body: CreateRecommendationData = bodyFactory()[0];
 
     const create = await prisma.recommendation.create({ data: { ...body } });
 
@@ -104,22 +93,10 @@ describe('GET /recommendations/top/:amount', () => {
   it('Should return list of top amount recommendations', async () => {
     const amount = 2;
 
-    const bodyScore10 = {
-      name: '10 Coisas IMPRESSIONANTES Que Encontrei na Internet!!',
-      youtubeLink: 'https://www.youtube.com/watch?v=m26jErLd5ds',
-      score: 10,
-    };
-    const bodyScore0 = {
-      name: 'Como ser um usuário Linux Avançado? - O que estudar exatamente?',
-      youtubeLink: 'https://www.youtube.com/watch?v=SAbyTbP2bjw',
-    };
-    const bodyScore05 = {
-      name: 'NO, pequeno e LEVE // MSI Modern 14',
-      youtubeLink: 'https://www.youtube.com/watch?v=Q5k-vp8OQjE',
-    };
+    const body = bodyFactory();
 
     await prisma.recommendation.createMany({
-      data: [{ ...bodyScore10 }, { ...bodyScore0 }, { ...bodyScore05 }],
+      data: [{ ...body[0], score: 10 }, { ...body[1] }, { ...body[2], score: 5 }],
     });
 
     const response = await supertest(app).get(`/recommendations/top/${amount}`);
@@ -132,10 +109,7 @@ describe('GET /recommendations/:id', () => {
   onStart();
 
   it('Should return an item by id', async () => {
-    const body: CreateRecommendationData = {
-      name: '10 Coisas IMPRESSIONANTES Que Encontrei na Internet!!',
-      youtubeLink: 'https://www.youtube.com/watch?v=m26jErLd5ds',
-    };
+    const body: CreateRecommendationData = bodyFactory()[0];
 
     const create = await prisma.recommendation.create({
       data: { ...body },
@@ -151,10 +125,7 @@ describe('POST /recommendations/:id/upvote', () => {
   onStart();
 
   it('Should return status 200 on upvote is done', async () => {
-    const body: CreateRecommendationData = {
-      name: '10 Coisas IMPRESSIONANTES Que Encontrei na Internet!!',
-      youtubeLink: 'https://www.youtube.com/watch?v=m26jErLd5ds',
-    };
+    const body: CreateRecommendationData = bodyFactory()[0];
 
     const create = await prisma.recommendation.create({
       data: { ...body },
@@ -178,10 +149,7 @@ describe('POST /recommendations/:id/downvote', () => {
   onStart();
 
   it('Should return status 200 on downvote is done', async () => {
-    const body: CreateRecommendationData = {
-      name: '10 Coisas IMPRESSIONANTES Que Encontrei na Internet!!',
-      youtubeLink: 'https://www.youtube.com/watch?v=m26jErLd5ds',
-    };
+    const body: CreateRecommendationData = bodyFactory()[0];
 
     const create = await prisma.recommendation.create({
       data: { ...body },
