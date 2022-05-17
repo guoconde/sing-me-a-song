664d5c2cac25f9a06455ba848792dedbecc0f805
Celso <celsolnv27@gmail.com>
2022-05-17 00:00:24 -0300

    Preparando revisão de código

--- back-end/src/routers/testRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import * as testController from '../controllers/testController.js';
-
-const testRouter = Router();
-
-testRouter.post('/reset', testController.resetDatabase);
-
-export default testRouter;
--- back-end/tests/utils/functionsUtils.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from '../../src/database.js';
-
-export async function prismaDisconnect() {
-  await prisma.$disconnect();
-}
-
-export async function truncateTable() {
-  await prisma.$executeRaw`TRUNCATE TABLE recommendations`;
-}
